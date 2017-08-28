1e0eaa977af52b00d708ba3de3a14b5d6da173a8
Julie Fukuyama <julia.fukuyama@gmail.com>
2017-08-28 16:22:20 -0700

    update testing functions

--- test_bowtie.py
+++ test_bowtie.py
@@ -3,55 +3,41 @@ import subprocess
 import pysam
 from Bio import SeqIO
 import os
-from string_compare import get_match_lengths
+from string_compare import get_match_properties
+from write_query_fq import write_query
+from merge_bowtie import merge_bowtie
 
 seed_length = 4
-reference = "test_data/seed_test.fa"
-query_fw = "test_data/seed_test_query_fw.fq"
-query_rc = "test_data/seed_test_query_rc.fq"
+reference = "test_data/lambda_virus.fa"
+query_prefix = "test_data/seed_test_query"
+output_prefix = "test_data/seed_test_output"
+query_sequence = "ACGCTGGCCATGC"
+write_query([query_sequence], ["r1"], [4], [4], query_prefix)
 # call the script that runs bowtie
 subprocess.call(["./bowtie_exact_match.sh",
-                 reference, query_fw, query_rc, str(seed_length)],
+                 reference, query_prefix, str(seed_length), output_prefix],
                 stdout=open(os.devnull, 'wb'),
                 stderr=open(os.devnull, 'wb'))
 
-ref_seq = SeqIO.read(reference, "fasta")
-samfile_fw = pysam.AlignmentFile("output_bt_fw.bam", "rb")
-samfile_rc = pysam.AlignmentFile("output_bt_rc.bam", "rb")
-ref_name = samfile_fw.references[0]
-
-# make a dictionary containing the forward reads indexed by their position
-fw_dict = {}
-for fw_read in samfile_fw.fetch(ref_name):
-    fw_dict[fw_read.get_reference_positions()[0]] \
-        = fw_read.get_reference_sequence()
-
-# make a dictionary with the merged reads indexed by their position
-merged_dict = {}
-for rc_read in samfile_rc.fetch(ref_name):
-    if rc_read.get_reference_positions()[-seed_length] in fw_dict.keys():
-        fw_seq = fw_dict[rc_read.get_reference_positions()[-seed_length]]
-        merged_seq = rc_read.get_reference_sequence() + fw_seq[seed_length:]
-        merged_dict[rc_read.get_reference_positions()[0]] = merged_seq
-    else:
-        # this should never happen
-        print("Error: couldn't find a match for rc read at position %i",
-              rc_read.get_reference_positions()[0])
+samfile_fw_right = pysam.AlignmentFile(output_prefix + "_fw_right.bam", "rb")
+samfile_fw_left = pysam.AlignmentFile(output_prefix + "_fw_left.bam", "rb")
+samfile_rc_right = pysam.AlignmentFile(output_prefix + "_rc_right.bam", "rb")
+samfile_rc_left = pysam.AlignmentFile(output_prefix + "_rc_left.bam", "rb")
+fw_merged_dict = merge_bowtie(samfile_fw_right, samfile_fw_left, seed_length)
+rc_merged_dict = merge_bowtie(samfile_rc_right, samfile_rc_left, seed_length)
 
 # get the match lengths from bowtie
-lengths_bt = [len(merged_dict[k]) for k in merged_dict.keys()]
+lengths_bt_fw = [len(fw_merged_dict[k]) for k in fw_merged_dict.keys()]
+lengths_bt_rc = [len(rc_merged_dict[k]) for k in rc_merged_dict.keys()]
 
 # get the match lengths from the simple function
-lengths = get_match_lengths(str(ref_seq.seq), query="AAAACCCCGGGG",
-                            seed_start=4, seed_len=seed_length)
+ref_seq = SeqIO.read(reference, "fasta")
+properties_fw = get_match_properties(str(ref_seq.seq), query=query_sequence,
+                                     seed_start=4, seed_len=seed_length)
+properties_rc = get_match_properties(str(ref_seq.seq), query=query_sequence,
+                                     seed_start=4, seed_len=seed_length, rc=True)
+lengths_fw = [l for (i, l, left, right) in properties_fw]
+lengths_rc = [l for (i, l, left, right) in properties_rc]
 
-# check that they are the same
-if sorted(lengths) == sorted(lengths_bt):
-    print("The set of match lengths for the two methods is the same")
-else:
-    print("The set of match lengths for the two methods is different")
-
-print("Lengths from simple function:")
-print(sorted(lengths))
-print("Lengths from bowtie2:")
-print(sorted(lengths_bt))
+print sorted(lengths_bt_fw) == sorted(lengths_fw)
+print sorted(lengths_bt_rc) == sorted(lengths_rc)
