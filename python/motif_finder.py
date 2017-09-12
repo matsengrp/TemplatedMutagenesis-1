789fd98b2c2e65f331a09584ab95e39c0de25203
Julia Fukuyama <julia.fukuyama@gmail.com>
2017-09-12 13:14:09 -0700

    add longest_motif_finder function and tests

--- python/motif_finder.py
+++ python/motif_finder.py
@@ -1,4 +1,5 @@
-
+from string_compare import extend_one_match
+import regex as re
 
 # For each (read, mutation) pair, looks at windows of seed_len around
 # the mutation and finds all the reference sequences that match.
@@ -33,5 +34,49 @@ def motif_finder(mutations, references, seed_len):
                 for r in references:
                     if seed in r:
                         reference_hits.add(r.name)
+            match_dict[(q, mut_idx)] = reference_hits
+    return match_dict
+
+
+def longest_motif_finder(mutations, references, seed_len):
+    """For each mutation, finds the longest matches in the references around
+    that mutation that match at least min_length bases.
+
+    Keyword arguments:
+    mutations -- A dictionary with query sequences as keys and a list
+    of mutation indices as values.
+    references -- A list of the reference sequences to match.
+    seed_len -- The length of the matched string
+
+    Returns: A dictionary keyed by (query, index) tuples with a set of
+    hits, in the form (ref_name, ref_seq, hit_index, hit_length)
+
+    """
+
+    match_dict = {}
+    for q in mutations.keys():
+        seq_len = len(q)
+        for mut_idx in mutations[q]:
+            # farthest left starting point for a window of length
+            # seed_len containing mut_idx
+            min_start = max(0, mut_idx - seed_len + 1)
+            # farthest right starting point for a window of length
+            # seed_len containing mut_idx
+            max_start = min(seq_len - seed_len, mut_idx)
+            # a set to store the hits in
+            reference_hits = set()
+            # get the hits for each window around the mutation
+            for r in references:
+                for start in range(min_start, max_start + 1):
+                    # create the seed
+                    seed = q[start:(start + seed_len)]
+                    # find all the places the seed matches the reference
+                    matches = [m.start() for m in re.finditer(seed, str(r.seq), overlapped=True)]
+                    # extend each of the matches that we found
+                    match_properties = [extend_one_match(r.seq, q, start, seed_len, m) for m in matches]
+                    for (match_idx, match_len, left_ext, right_ext) in match_properties:
+                        # store the reference name, its sequence, the
+                        # starting index of the match, and the length
+                        reference_hits.add((r.name, str(r.seq), match_idx, match_len))
             match_dict[(q, mut_idx)] = reference_hits
     return match_dict
--- python/test_motif_finder.py
+++ python/test_motif_finder.py
@@ -2,7 +2,7 @@ import unittest
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from motif_finder import motif_finder
-
+from motif_finder import longest_motif_finder
 
 class testMF(unittest.TestCase):
 
@@ -53,6 +53,54 @@ class testMF(unittest.TestCase):
         self.assertEqual(mf_out[(q1, 1)], set(["r1"]))
         self.assertEqual(mf_out[(q1, 4)], set())
         self.assertEqual(mf_out[(q2, 5)], set(["r1"]))
+
+
+class testLMF(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        # set up the test data
+        match = "ATGCCCC"
+        query = "GGGGG" + match + "GGGGG"
+        # r1 is the sequence that has a match to the query
+        r1 = SeqRecord(Seq("AAAAAAAA" + match + "AAAAAAAA"),
+                       name="r1")
+        # r2 is a decoy sequence
+        r2 = SeqRecord(Seq("AAAAAAAAAAAAAAAAAAAA"),
+                       name="r2")
+        # we want to find a match to the query around position 7
+        mut_idx = 7
+        mut_map = {query: [mut_idx]}
+        ref = [r1, r2]
+        seed_len = 3
+        # run longest_motif_finder
+        lmf_out = longest_motif_finder(mut_map, ref, seed_len)
+        # the query centered at 7 matches to 1 position in the
+        # reference
+        self.assertEqual(len(lmf_out[(query, mut_idx)]), 1)
+        # the match should start at position 8 and go for the
+        # entire length of 'match'
+        self.assertEqual(lmf_out[(query, mut_idx)],
+                         set([("r1", str(r1.seq), 8, len(match))]))
+
+    def test_overlapping(self):
+        # set up the test data
+        match = "A" * 10
+        query = "G" * 10 + match + "G" * 10
+        r1 = SeqRecord(Seq("C" * 10 + match + "C" * 10),
+                       name="r1")
+        mut_idx = 15
+        mut_map = {query: [mut_idx]}
+        ref = [r1]
+        seed_len = 4
+        # run longest_motif_finder
+        lmf_out = longest_motif_finder(mut_map, ref, seed_len)
+        # there should be a match to r1, starting at position 10, of
+        # length len(match)
+        self.assertEqual(("r1", str(r1.seq), 10, len(match)) in
+                         lmf_out[(query, mut_idx)], True)
 
 
 if __name__ == '__main__':
