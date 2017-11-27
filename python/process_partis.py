15a898aebcbfc124c4e4e75ebd7ccb10254d7259
Julia Fukuyama <julia.fukuyama@gmail.com>
2017-11-27 08:46:01 -0800

    comments for process_partis.py

--- python/process_partis.py
+++ python/process_partis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
@@ -7,22 +8,37 @@ from Bio.SeqRecord import SeqRecord
 # now it just compares the naive sequence to the indel reversed
 # sequences
 def process_partis(partis_file):
+    """Reads in mutations from a partis file
+
+    Keyword arguments:
+    partis_file -- A string giving the name of a partis file.
+
+    Returns: A data frame with one row per mutation. Each row contains
+    the mutated sequence, naive sequence, sequence id, the location of
+    the mutation, the germline base, and the mutated base.
+
+    """
     annotations = pd.read_csv(partis_file)
+    # a list to store the rows of the data frame in
     mutation_rows = []
-    row_count = annotations.shape[0]
-    for row in range(row_count):
+    # step through the partis data frame row by row
+    for row in range(annotations.shape[0]):
         naive_seq = annotations["naive_seq"][row]
+        # we call mutations from the indel reversed sequences
         indel_reversed_seq = annotations["indel_reversed_seqs"][row]
+        # if no annotation, skip the sequence
         if pd.isnull(naive_seq):
             continue
+        # no indel reversed sequence means that there were no indels
+        # to reverse and we should use the input sequence
         if pd.isnull(indel_reversed_seq):
             mutated_seq = annotations["input_seqs"][row]
         else:
             mutated_seq = indel_reversed_seq
-        mutated_seq_len = len(mutated_seq)
-        mutation_idx = [i for i in range(mutated_seq_len) if
-                        mutated_seq[i] != naive_seq[i]]
-        for i in range(mutated_seq_len):
+        # search along the sequence for mutations (mismatches between
+        # naive and indel reversed sequence) and make one row per
+        # mutation
+        for i in range(len(mutated_seq)):
             if(mutated_seq[i] != naive_seq[i]):
                 mutation_rows.append({
                         "mutated_seq": mutated_seq,
@@ -36,17 +52,34 @@ def process_partis(partis_file):
     return(mutation_df)
 
 
-def process_partis_poly(partis_file, min_spacing):
+def process_partis_poly(partis_file, max_spacing):
+    """Gets pairs of neighboring mutations for PolyMotifFinder.
+
+    Keyword arguments:
+    partis_file -- A string giving the name of a partis file.
+    max_spacing -- The maximum distance allowed between mutations.
+
+    Returns: A data frame. Each row corresponds to a pair of mutations
+    within max_spacing of each other and contains the mutated
+    sequence, naive sequence, sequence id, mutation indices, germline
+    bases, and mutated bases.
+
+    """
     # first get all the single mutations using process_partis
     mutation_df = process_partis(partis_file)
+    # a list to hold the rows of the data frame
     poly_mutation_rows = []
-    mutation_df_set = set(mutation_df["mutated_seq_id"]) 
-    for seq_id in mutation_df_set:
+    # loop through the sequences
+    sequence_ids = set(mutation_df["mutated_seq_id"]) 
+    for seq_id in sequence_ids:
+        # just the mutations corresponding to the current sequence
         mutation_df_subset = mutation_df[mutation_df.mutated_seq_id == seq_id]
+        # get all the pairs of mutations within max_spacing of each other
         poly_mutations = get_pairs(mutation_df_subset["mutation_index"], \
-                min_spacing)
+                max_spacing)
         mutated_seq = mutation_df_subset["mutated_seq"][0]
         naive_seq = mutation_df_subset["naive_seq"][0]
+        # for each pair of mutations, create a row describing those mutations
         for pm in poly_mutations:
             poly_mutation_rows.append({
                     "mutated_seq": mutated_seq,
@@ -59,12 +92,25 @@ def process_partis_poly(partis_file, min_spacing):
     return(pd.DataFrame(poly_mutation_rows))
 
 
-def get_pairs(mut_indices, min_spacing):
+def get_pairs(mut_indices, max_spacing):
+    """Finds pairs of mutations within max_spacing of each other
+
+    Keyword arguments:
+    mut_indices -- A list containing mutation indices.
+    max_spacing -- The maximum distance between pairs of mutations.
+
+    Returns: A list containing all pairs of mutations within
+    max_spacing of each other.
+
+    """
+    # a list to hold the pairs
     pairs = []
     mut_indices_len = len(mut_indices)
+    # loop through all the pairs of mutations, put them in the list if
+    # they are within max_spacing of each other.
     for i in range(0, mut_indices_len - 1):
         for j in range(i+1, mut_indices_len):
-            if mut_indices[j] - mut_indices[i] <= min_spacing:
+            if np.abs(mut_indices[j] - mut_indices[i]) <= max_spacing:
                 pairs.append((mut_indices[i], mut_indices[j]))
             else:
                 continue
--- python/test_process_partis.py
+++ python/test_process_partis.py
@@ -1,5 +1,5 @@
 import unittest
-from process_partis import get_pairs, unseen_mutations, process_partis, process_partis_poly
+from process_partis import get_pairs, process_partis, process_partis_poly
 from motif_finder import likelihood_given_gcv
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
@@ -26,7 +26,7 @@ class testPP(unittest.TestCase):
     def test_process_partis_poly(self):
         # run partis on the test data
         partis_file = "../test_data/partis_poly_test.csv"
-        mut_df = process_partis_poly(partis_file, min_spacing=2)
+        mut_df = process_partis_poly(partis_file, max_spacing=2)
         self.assertEqual(mut_df.shape[0], 1)
         self.assertEqual(mut_df["mutated_seq"][0], "AAAAAAAA")
         self.assertEqual(mut_df["naive_seq"][0], "GAAAAGAG")
@@ -39,7 +39,7 @@ class testPP(unittest.TestCase):
         # set up the test data
         mut_indices = [0, 1, 8, 9, 10, 15, 18, 21, 23]
         # run get_pairs
-        pairs = get_pairs(mut_indices, min_spacing=2)
+        pairs = get_pairs(mut_indices, max_spacing=2)
         # check that 
         self.assertEqual((0, 1) in pairs, True)
         self.assertEqual((8, 9) in pairs, True)
@@ -47,48 +47,6 @@ class testPP(unittest.TestCase):
         self.assertEqual((9, 10) in pairs, True)
         self.assertEqual((21, 23) in pairs, True)
         self.assertEqual(len(pairs), 5)
-
-
-class testUnseenMutations(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_correct_mutations(self):
-        partis_file = "../test_data/partis_test.csv"
-        mm = unseen_mutations(partis_file)
-        unseen_bases = set([k[mm[k][0]] for k in mm.keys()])
-        unseen_seq_1 = Seq("AAAAAAAC")
-        unseen_seq_2 = Seq("AAAAAAAT")
-        # there is one mutated base, so there are two corresponding
-        # unseen mutations
-        self.assertEqual(len(mm), 2)
-        # the unseen bases should be T and C
-        self.assertEqual(unseen_bases, set(["T", "C"]))
-        # the sequences should be AAAAAAAC and AAAAAAAT
-        self.assertEqual(set([k.seq for k in mm.keys()]),
-                         set([unseen_seq_1, unseen_seq_2]))
-        # the ids for both of the sequences should be s1
-        self.assertEqual(set([k.id for k in mm.keys()]),
-                         set(["s1"]))
-
-    def test_correct_mutations_indels(self):
-        partis_file = "../test_data/partis_test_indels.csv"
-        mm = unseen_mutations(partis_file)
-        unseen_bases = set([k[mm[k][0]] for k in mm.keys()])
-        unseen_seq_1 = Seq("AAAAAAAC")
-        unseen_seq_2 = Seq("AAAAAAAT")
-        # there is one mutated base, so there are two corresponding
-        # unseen mutations
-        self.assertEqual(len(mm), 2)
-        # the unseen bases should be T and C
-        self.assertEqual(unseen_bases, set(["T", "C"]))
-        # the sequences should be AAAAAAAC and AAAAAAAT
-        self.assertEqual(set([k.seq for k in mm.keys()]),
-                         set([unseen_seq_1, unseen_seq_2]))
-        # the ids for both of the sequences should be s1
-        self.assertEqual(set([k.id for k in mm.keys()]),
-                         set(["s1"]))
 
 
 if __name__ == '__main__':
